8089ed36a700f856abcaf1faee9bfb9fc6df47f0
Matt Spilman <mspilman@Matts-MacBook-Pro.local>
2020-08-29 15:41:56 -0500

    just commenting things out

--- src/store/add-product/reducers.js
+++ src/store/add-product/reducers.js
@@ -1,22 +1,22 @@
-import actions from './action-types';
+// import actions from './action-types';
 
-const product = {};
-const initialState = product ? { added: false, product } : {};
+// const product = {};
+// const initialState = product ? { added: false, product } : {};
 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case actions.ADD_PRODUCT_REQUEST:
-      return {
-        adding: true,
-      };
-    case actions.ADD_PRODUCT_SUCCESS:
-      return {
-        adding: true,
-        product: action.product,
-      };
-    case actions.ADD_PRODUCT_FAILURE:
-      return {};
-    default:
-      return state;
-  }
-};
+// export default (state = initialState, action) => {
+//   switch (action.type) {
+//     case actions.ADD_PRODUCT_REQUEST:
+//       return {
+//         adding: true,
+//       };
+//     case actions.ADD_PRODUCT_SUCCESS:
+//       return {
+//         adding: true,
+//         product: action.product,
+//       };
+//     case actions.ADD_PRODUCT_FAILURE:
+//       return {};
+//     default:
+//       return state;
+//   }
+// };
